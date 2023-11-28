77bc6b0ef7270a4a4c4cbb806e81274e5ad9a93c
Ali Hussnain <alihussnainrrb@gmail.com>
2023-11-28 11:26:59 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,21 @@
 import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 import { ApiResponse } from "./typing/api";
-import { SessionUser } from "./typing/auth";
+import { PublicUser } from "./typing/auth";
 
 const unauthorizedRes: ApiResponse<null> = {
   succeed: false,
   code: "UNAUTHORIZED",
 };
 
-
 // export default function middleware() {
 
 // }
 
-
 export default withAuth(
   function middleware(req: NextRequestWithAuth) {
     const { pathname } = req.nextUrl;
-    const user = (req.nextauth.token as { user: SessionUser | null }).user;
+    const user = (req.nextauth.token as { user: PublicUser | null }).user;
     if (!user) return NextResponse.redirect(new URL("/signin", req.url));
   },
   {
